4f5bb3c06d771598633afb4d60bae3a90dff8003
Esperlluette <janl.mannessier@gmail.com>
2022-09-18 16:44:09 +0200

    génération ok, calcul ok;

--- script.js
+++ script.js
@@ -6,55 +6,183 @@
  */
 
 
+/**
+ * lorsque j'affiche les résultats et que je 
+ * réinitialise le tableur les lignes total se dupliquent
+ */
+
+
+var tableX = 4;
+var tableY = 4;
 var table = document.getElementById('table');
 
 var resetB = document.getElementById("reset");
 var calculB = document.getElementById("calcul");
 
+var boolResults = false;
 
-resetB.addEventListener("click", () => { vanish(table) }, false);
+
+resetB.addEventListener("click", () => { vanish() }, false);
 
 
 calculB.addEventListener("click", () => { calcul(table) }, false);
 
 
 
-function vanish(table) {
+function vanish() {
     /**
      * @params Table HTML : table
      * 
      * - vide le tableau passé en paramètres et le 
-     *   re-rempli avec la valeur par défaut
+     *   re-rempli avec la valeur par défaut (0)
      */
+    var collection = document.getElementsByClassName('div');
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].childNodes[0].nodeValue = "1"
+    }
+    console.log("Tableau_réinitialisé \nbip boup");
+    boolResults = false;
+}
 
+function calcul() {
 
+    var collection = document.getElementsByClassName('div');
+    var tCollection = getAllValue(collection);
+    var tSortCollection = sortTable(tCollection);
+    var resultsVertical = calculVertical(tSortCollection);
+    var resultHorizontal = calculHorizontal(tSortCollection);
+
+    // console.table(resultsVertical);
+    // console.table(resultHorizontal);
+    if (!boolResults){
+    resultToScreen(resultsVertical, resultHorizontal, table);
+    } else console.log('nope');
 
 }
 
-function calcul(table) {
-    getAllValue(table)
+function resultToScreen(resultsY, resultsX, table) {
 
 
-    for (let i = 2; i < table.length*2; i++) {
-        for (let j = 2; j < table.length*2; j++) {
+    //résultats horizontaux ok
+    //résultats verticaux Pas ok 
+    /**
+     * @params Array : resultsV
+     * @params Array : resultH
+     * @return void
+     * 
+     * - Affiche les résultats totaux 
+     */
 
+    j=0;
+    for (let i = 2; i <= tableY*2; i+=2) {
+            console.log(i);
+            var td = document.createElement("td");
+
+
+            var div = document.createElement("div");
+            div.className = "div";
+            div.id = "total "+  i;
+            div.innerHTML =  resultsX[j];
+
+            td.appendChild(div);
+
+            table.childNodes[1].childNodes[i].appendChild(td);
+     j++;
+    }
+    boolResults = true;
+}
+
+function calculHorizontal(tSortCollection) {
+
+    /**
+     * @params Array : tCollection
+     * @return Array : results
+     * 
+     * - récupère un tableau de valeurs et calcule
+     *   tous les totals horizontaux pour les 
+     *   retourner sous forme de tableau.
+     */
+
+    var results = [];
+    for (let i = 0; i < tableY; i++) {
+
+        var temp = 0;
+        for (let j = 0; j < tableX; j++) {
+            temp = temp + parseInt(tSortCollection[i][j]);
+        }
+        results.push(temp);
+    }
+
+    return results;
+
+}
+
+function calculVertical(tSortCollection) {
+    /**
+     * @params Array2D : tSortCollection
+     * @return Array : results
+     * 
+     * - récupère un tableau de valeurs et calcule
+     *   tous les totals verticaux pour les 
+     *   retourner sous forme de tableau.
+     */
+    var results = [];
+    for (let i = 0; i < tableY; i++) {
+
+        var temp = 0;
+        for (let j = 0; j < tableX; j++) {
+            temp = temp + parseInt(tSortCollection[j][i]);
+        }
+        results.push(temp);
+    }
+
+    return results;
+
+}
+
+function sortTable(tCollection) {
+
+    /**
+     * @params Array : tCollection
+     * @return Array2D : tSortCollection 
+     * 
+     * - récupère un tableau en une dimention
+     *   pour le retourner sous forme d'un tableau 2D
+     */
+
+    var values = [];
+    var temp = [];
+    for (let i = 0; i < tCollection.length; i++) {
+        temp.push(tCollection[i]);
+
+        if (temp.length == tableX) {
+            values.push(temp);
+            temp = [];
         }
     }
 
+    return values;
+
 }
 
 
-function getAllValue(table) {
+function getAllValue(collection) {
     /**
-     * @params Table HTML : table
+     * @params Collection HTML : table
      * @return Array : values
      * 
-     * - récupère toutes les valeurs du tableau pour les retourner dans une liste.
+     * - récupère toutes les valeurs du tableau 
+     *   pour les retourner dans tableau.
      */
 
+    var value = [];
 
-    
+    for (let i = 0; i < collection.length; i++) {
 
+        value.push(collection[i].childNodes[0].nodeValue)
+
+    }
+    return value;
 }
 
 divGenerator(table);
@@ -69,8 +197,8 @@ function divGenerator(table) {
      * - crée renpli un tableau HTML vide avec des td>div 
      */
 
-    for (let i = 2; i <= 8; i = i + 2) {
-        for (let j = 2; j <= 8; j = j + 2) {
+    for (let i = 2; i <= tableY * 2; i = i + 2) {
+        for (let j = 2; j <= tableX * 2; j = j + 2) {
 
 
             var td = document.createElement("td");
@@ -78,7 +206,8 @@ function divGenerator(table) {
 
             var div = document.createElement("div");
             div.className = "div";
-            div.innerHTML = " 1 ";
+            div.id = i + " " + j;
+            div.innerHTML = "1";
             div.setAttribute("contenteditable", "true");
 
             td.appendChild(div);
